ab4fa5e44643ca44fb5344cbedd370a7a7f8c11b
markbaltic <mark.baltic@gmail.com>
2016-01-16 19:28:24 +0100

    Update ryanair.py

--- ryanair.py
+++ ryanair.py
@@ -7,9 +7,9 @@ r = requests.get("https://desktopapps.ryanair.com/en-gb/availability?ADT=1&CHD=0
                  +"Destination={0}&FlexDaysOut=2&INF=0&"
                  +"Origin=TRS&RoundTrip=false&TEEN=0".format(stn)
     )
-print(r.text)
+print(r.textaa)
 
-podatki = json.loads(r.twefwefwefwefwefwefewext)
+podatki = json.loads(r.fwefewext)
 ##
 ##if len(podatki['trips'][0]['dates'][0]['flights'])==0:
 ##    print("Ni leta")
